dde398d4dacb911ade0aafa2680a67326be7cb91
Austin Adams <adamsaustin104@gmail.com>
2020-02-13 10:05:22 -0800

    Update Weather.js
    Added props for loading and inputs.  Set default location to show up for SLC.
    
    created functions to handle updates to input and new location information

--- InterviewProject.Web/ClientApp/src/components/Weather.js
+++ InterviewProject.Web/ClientApp/src/components/Weather.js
@@ -1,79 +1,84 @@
-import React, { Component } from 'react';
-import { WeatherData } from './WeatherData';
+import React, { Component }          from 'react';
+import WeatherData                   from './WeatherData';
+import Loading                       from './Loading';
+import Autocomplete                  from "./Autocomplete";
+// import { AsyncTypeahead, Typeahead } from 'react-bootstrap-typeahead';
 
 export class Weather extends Component {
     static displayName = Weather.name;
 
-    state = {
+    constructor(props){
+      super(props)
+
+      this.state = {
+        loading: true,
         forecasts: [],
-        loading: true
-    };
-  
+        input: '',
+        activeLocation: '2487610',
+        possibleCities: [],
+      }
+
+      this.handleNewLocation = this.handleNewLocation.bind(this)
+      this.updateInput = this.updateInput.bind(this)
+    }
+        
+    updateInput(e){
+      const value = e.target.value
+
+      if(value !== null){
+        this.setState({
+          input: value
+        })
+      }
+    }
+
+    handleNewLocation(location){
+      console.log("handle new location called")
+      this.setState({
+        activeLocation: location,
+      })
+      console.log(this.state.activeLocation)
+    }
+
+    populateWeatherData(location) {
+      console.log("populate weather data")
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      const fullUrl = proxyurl + `https://www.metaweather.com/api/location/${location}`;
+      
+      fetch(fullUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          forecasts: data,
+          loading: false,
+        })
+      })
+      .catch((error) => console.log(error));
+    }
 
   componentDidMount() {
-    this.populateWeatherData();
+    this.populateWeatherData(this.state.activeLocation);
   }
 
-  static renderForecastsTable(forecasts) {
-      return (
-          console.log(forecasts),
-          <div class="card-columns">
-              <div class="card">
-                  <div class="card-img">
-                  </div>
-                  <div class="card-body">
-                      <h5 class="card-title">@product.Title</h5>
-                  </div>
-                  <div class="card-footer">
-                  </div>
-              </div>
-          </div >,
-
-
-        <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td></td>
-                <td>{forecasts.consolidated_weather[0].created}</td>
-                <td>{forecasts.consolidated_weather[0].the_temp}</td>
-                <td>{forecasts.consolidated_weather[0].the_temp}</td>
-                <td>{forecasts.summary}</td>
-            </tr>
-        </tbody>
-      </table >
-     );
-  }
 
   render() {
     let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : Weather.renderForecastsTable(this.state.forecasts);
+      ? <Loading />
+      : <WeatherData forecast={this.state.forecasts}/>;
 
     return (
       <div>
-        <h1 id="tabelLabel" >Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
+        <Autocomplete handleNewLocation={this.handleNewLocation}
+        /> 
+        <br />
+        <button onClick={() => 
+          this.populateWeatherData(this.state.activeLocation)}>
+            Submit
+        </button>
         {contents}
       </div>
     );
-  }
-    //https://www.metaweather.com/static/img/weather/c.svg images
-    async populateWeatherData() {
-        const proxyurl = "https://cors-anywhere.herokuapp.com/";
-        const response = await fetch(proxyurl + 'https://www.metaweather.com/api/location/44418');
-        const data = await response.json();
-        
-        this.setState({
-            forecasts: data,
-            loading: false
-        });
   }
 }
