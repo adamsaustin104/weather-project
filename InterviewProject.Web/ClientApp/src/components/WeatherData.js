904c98182902b442906888d18983f307a997a1ea
Austin Adams <adamsaustin104@gmail.com>
2020-02-13 09:57:52 -0800

    Restructured weather data.js
    Changed component to render cards showing the 5 day forecast.

--- InterviewProject.Web/ClientApp/src/components/WeatherData.js
+++ InterviewProject.Web/ClientApp/src/components/WeatherData.js
@@ -1,20 +1,31 @@
 ﻿import React from 'react'
 
-export const WeatherData = ({
-    id = "No Id",
-    weather_state = "Clear",
-    weather_State_Abbr = "c",
-    date = Date.now(),
-    max_Temp = 50,
-    min_Temp = 0,
-    the_Temp = 25,
-    title = "Today"}) => {
-    return (
-        <section>
-            <h2>{title}</h2>
-            <img alt={weather_state} src='https://www.metaweather.com/static/img/weather/c.svg'/>
-            <p>High: {max_Temp}</p>
-            <p>Low: {min_Temp}</p>
-        </section>
-    )
-}
+const WeatherData = (props) => (
+  <div>
+    <h2 style={{textAlign: 'center'}}>{props.forecast.title}</h2>
+    <hr />
+      <div className="card-columns">
+        {props.forecast.consolidated_weather.map(
+          ({ id, applicable_date, weather_state_name, weather_state_abbr, max_temp, min_temp, the_temp}) => 
+          (
+          <div className='card' key={id}>
+            <div className='card-header'><h5>{new Date(applicable_date).toLocaleDateString(undefined, {weekday: 'short', month: 'short', day: 'numeric'} )}</h5>
+            </div>
+            <div className='card-img' style={
+              {backgroundImage: `url(${`https://www.metaweather.com/static/img/weather/${weather_state_abbr}.svg`})`}
+              }>
+            </div>
+            <div className='card-footer'>
+              {(the_temp === props.forecast.consolidated_weather[0].the_temp)
+              ? <h5>Current: {(32 + (the_temp / 0.5556)) | 0}˚F</h5>
+              : <br />}
+              <p>High: {(32 + (max_temp / 0.5556)) | 0}˚F</p>
+              <p>Low: {(32 + (min_temp / 0.5556)) | 0}˚F</p>
+            </div>  
+          </div>
+        ))}
+      </div>
+  </div>
+)
+
+export default WeatherData;
